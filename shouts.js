cf10acb2381fe85a5f35a9695a1a258823f54cfd
Brandon <bleventh@gmail.com>
2013-08-01 23:59:08 -0700

    Fixed errors in shout.js
    There were a lot.

--- shouts.js
+++ shouts.js
@@ -1,17 +1,17 @@
-modules.exports = function(app) {
+module.exports = function(app) {
    app.get('/shouts', function(req, res) {
-      res.send(JSON.stringfy({'thisIsA': 'shout!', 'thisIsAnother':'shout2!'}));
-   }
+      res.send(JSON.stringify({'thisIsA': 'shout!', 'thisIsAnother':'shout2!'}));
+   });
+
+   app.get('/shouts/:id', function(req, res) {
+      res.send('hola');
+   });
 
    app.post('/shouts', function(req,res) {
-
-   }
-
-   app.get('/shouts/:id') {
-
-   }
+      res.send('hello post');
+   });
 
    app.post('/comment/:id', function(req,res) {
-
-   }
+      res.send('hold ma dick');
+   });
 }
