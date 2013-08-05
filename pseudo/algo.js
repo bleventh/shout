04c76cfd87faa819801b0ff8442d3a603fbcb770
Andrew Giel <agiel@stanford.edu>
2013-08-04 21:36:21 -0700

    Changed to MagicDensity->MagicNumPeople. Added Initial Radius.
    Incomplete see comments

--- pseudo/algo.js
+++ pseudo/algo.js
@@ -3,29 +3,26 @@ Think of this file like psuedocode
 */
 
 var INIT_RADIUS;
-var MAGIC_DENSITY;
+var MAGIC_NUM_PEOPLE;
 var LENIENCY_PERCENTAGE;
 var MIN_RADIUS;
 var SCALING_PERCENT;
 var TIME_ADJUST;
-var UPPER_ACCEPT = MAGIC_DENSITY + MAGIC_DENSITY*LENIENCY_PERCENTAGE;
-var LOWER_ACCEPT = MAGIC_DENSITY - MAGIC_DENSITY*LENIENCY_PERCENTAGE;
+var UPPER_ACCEPT = MAGIC_NUM_PEOPLE + MAGIC_NUM_PEOPLE*LENIENCY_PERCENTAGE;
+var LOWER_ACCEPT = MAGIC_NUM_PEOPLE - MAGIC_NUM_PEOPLE*LENIENCY_PERCENTAGE;
 var TIME_CONSTANT;
 var SCORE_SCALE;
 
 function getInitialHorizon(shout) {
 	var radius = INIT_RADIUS;
 	var shoutsInArea = getAllShouts(shout.location, radius);
-	var density = shoutsInArea/((radius^2)*PI);
-	while ((density > UPPER_ACCEPT || density < LOWER_ACCEPT) && radius > MIN_RADIUS) {
-		if (density > UPPER_ACCEPT) {
+	while ((shoutsInArea > UPPER_ACCEPT || shoutsInArea < LOWER_ACCEPT) && radius > MIN_RADIUS) {
+		if (shoutsInArea > UPPER_ACCEPT) {
 			radius*=SCALING_PERCENT;
 			shoutsInArea = getAllShouts(shout.location, radius);
-			density = shoutsInArea/((radius^2)*PI);
-		} else if (density < LOWER_ACCEPT) {
+		} else if (shoutsInArea < LOWER_ACCEPT) {
 			radius/=SCALING_PERCENT;
 			shoutsInArea = getAllShouts(shout.location, radius);
-			density = shoutsInArea/((radius^2)*PI);
 		}
 	}
 	if (radius < MIN_RADIUS) radius = MIN_RADIUS;
@@ -44,7 +41,7 @@ function updateHorizon(shout){
 }
 
 function getHotRank(shout) {
-	var timeSincePost = System.timeMillis()-shout.getcreationTime();
+	var timeSincePost = TIME_NOW-shout.creationTime;
 	var popularityScore = shout.amps - shout.muffles;
 	var sign;
 	if (popularityScore > 0) sign = 1;
@@ -52,6 +49,7 @@ function getHotRank(shout) {
 	else sign = 0; 
 	var maxVal;
 	if (popularityScore != 0) maxVal = abs(popularityScore);
-	else maxVal = 0; 
-	var order = log(maxVal) + (sign*timeSincePost)/(TIME_CONSTANT)
+	else maxVal = 1; 
+	var hotRank = Math.log(maxVal) + (sign*timeSincePost)/(TIME_CONSTANT);
+	return hotRank; // for debug
 } 
